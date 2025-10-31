aeb93a6313e82374541fb333532b9f8d8b24201f
Eami Yoma <116334758+EamiYoma@users.noreply.github.com>
2025-10-31 14:48:08 +0600

    Add files via upload

---
+++ countingsort.py
@@ -0,0 +1,15 @@
+def counting_sort(arr):
+    if not arr:
+        return arr
+    max_val = max(arr)
+    count = [0] * (max_val + 1)
+    for num in arr:
+        count[num] += 1
+    sorted_arr = []
+    for i in range(len(count)):
+        sorted_arr.extend([i] * count[i])
+    return sorted_arr
+
+# Example
+arr = [4, 2, 2, 8, 3, 3, 1]
+print(counting_sort(arr))  # Output: [1, 2, 2, 3, 3, 4, 8]
